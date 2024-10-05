71c647e5fe168193cd5f350a5144098af493e2bb
fatik07 <fatichur.r07@gmail.com>
2024-10-05 16:10:16 +0700

    fix: ketika data bacotan null

--- resources/views/pages/recent-content.blade.php
+++ resources/views/pages/recent-content.blade.php
@@ -1,14 +1,20 @@
 <div class="mt-8">
     <div class="flex justify-center gap-7 items-center flex-wrap">
-        @foreach ($curhats as $curhat)
+        @forelse ( $curhats as $curhat )
         @include('components.card', ['curhat' => $curhat])
-        @endforeach
+        @empty
+        <div class="text-center text-red-500">
+            Data Bacotanmu Belum ada nihhh.
+        </div>
+        @endforelse
     </div>
-    <div class="flex justify-center items-center mt-5">
+
+    @if ( $curhats->count() >= 1) <div class="flex justify-center items-center mt-5">
         <x-a-link url="/curhat-semua" color="bg-secondary-700 text-white hover:bg-secondary-500 font-comfortaa">
             Lihat Semua
         </x-a-link>
     </div>
+    @endif
 </div>
 
 <x-alert></x-alert>
